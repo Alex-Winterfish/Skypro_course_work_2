7845547ecb85b2b379570b8f3bfd367cac8f1c40
Алексей <alksbulgakov@gmail.com>
2024-11-13 19:54:04 +0300

    написан класс для работы с файлами

--- src/vacancy_processing.py
+++ src/vacancy_processing.py
@@ -1,4 +1,6 @@
 import json
+import re
+
 
 class VacancyProc:
     vacancy_name: str
@@ -58,11 +60,10 @@ class VacancyProc:
 
 
 class FileWriteJson:
-
     vacancy_id = 1
 
-    def __init__(self, vacancy:VacancyProc, file_path=None):
-        self.file_path = file_path
+    def __init__(self, vacancy:VacancyProc, file_path='../data/vacancies.json'):
+        self.__file_path = file_path
         self.name = vacancy.vacancy_name
         self.salary = vacancy.salary
         self.requirement = vacancy.requirement
@@ -70,24 +71,79 @@ class FileWriteJson:
         self.schedule = vacancy.schedule
         self.vacancy_dict = dict()
 
+
     @property
     def json_write(self):
         'Метод формирует словарь для добавления в файл'
-        self.vacancy_dict['id вакансии'] = FileWriteJson.vacancy_id
         self.vacancy_dict['Название Вакансии'] = self.name
         self.vacancy_dict['Требования к вакансии'] = self.requirement
         self.vacancy_dict['Зарплата'] = self.salary
         self.vacancy_dict['Ссылка на вакансию НН.ru'] = self.vacancy_url
-        with open(self.file_path, 'a') as f:
-            json.dump(self.vacancy_dict, f, ensure_ascii=False, indent=-1, separators=(',', ':'))
+
+        stop = 0
+        with open(self.__file_path, 'r') as file:
+            try:
+                data = json.load(file)
+                for vacancy in data:
+                    if vacancy.get('Название Вакансии') == self.name:
+                        stop = 1
+                        print('Vacancy already exist')
+                    else:
+                        continue
+
+            except json.decoder.JSONDecodeError:
+                data = list()
+
+        if stop == 0:
+            with open(self.__file_path, 'w+') as file:
+                data.append(self.vacancy_dict)
+                json.dump(data, file, ensure_ascii=False, indent=1)
+
         FileWriteJson.vacancy_id += 1
 
-    @property
-    def json_delite(self):
 
-        with open(self.file_path, 'r') as f:
-            data = json.load(f)
-        return data
+
+
+
+    def json_delite(self, search_string:str):
+
+        try:
+            with open(self.__file_path, 'r+') as f:
+                data = json.load(f)
+            for vacancy in data:
+                for key, value in vacancy.items():
+                    if key not in ['Зарплата']:
+                        search_pattern = re.search(f'{search_string}', value, flags=re.I)
+                        if search_pattern is not None:
+                            data.remove(vacancy)
+
+            with open(self.__file_path, 'w') as f:
+                json.dump(data, f, ensure_ascii=False, indent=-1, separators=(',', ':'))
+        except Exception as e:
+            print(f'Ошибка {e} в модуле json_delite')
+
+
+    def json_get(self,search_string:str):
+
+        try:
+            with open(self.__file_path, 'r') as file:
+                data = json.load(file)
+
+            for vacancy in data:
+                for key, value in vacancy.items():
+                    if key not in ['Зарплата']:
+                        search_pattern = re.search(f'{search_string}', value, flags=re.I)
+                        if search_pattern is not None:
+                            return vacancy
+                        else:
+                            print('Вакансия не найдена')
+        except Exception as e:
+            print(f'Ошибка {e} в модуле json_get')
+
+
+
+
+
 
 
 
@@ -96,8 +152,25 @@ if __name__ == '__main__':
 
     vacancy1 = VacancyProc('монтер', 'kghkh', 'sdfd', 'sfsf')
     vacancy2 = VacancyProc('механик', 'kghkh', 'sdfd', 'sfsf', 30000)
+    vacancy3 = VacancyProc.new_vacancy(["Младший программист C++ (Windows, UNIX)",
+    130000,
+    "https://api.hh.ru/vacancies/109847610?host=hh.ru",
+    "Понимание принципов объектно-ориентированного программирования. Опыт работы на железнодорожном транспорте в части систем управления сигнализацией, централизацией и блокировкой (<highlighttext>СЦБ</highlighttext>), и...",
+    "Электромеханик СЦБ",
+    'полный день'])
+
+    vacancy4 = VacancyProc('уборщик','cleaning', 'sdfdsf', 'sdfd')
+
+    #file_vacancy = FileWriteJson(vacancy2, '../data/vacancies.json').json_write
+    file_vacancy1 = FileWriteJson(vacancy1)
+    file_vacancy2 = FileWriteJson(vacancy2)
+    file_vacancy3 = FileWriteJson(vacancy3)
+    file_vacancy4 = FileWriteJson(vacancy4)
 
 
-    file_vacancy = FileWriteJson(vacancy1, '../data/vacancies.json').json_write
+    #print(file_vacancy)
+    file_vacancy1.json_delite('монтер')
+    file_vacancy2
+    print(file_vacancy3.json_get('механик'))
+    print(file_vacancy4.vacancy_id)
 
-    #print(file_vacancy.json_delite)
